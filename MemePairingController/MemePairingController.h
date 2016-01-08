13e2962b9f72ebc3c9c80f5c75225e2af206e871
c-bata <contact@c-bata.link>
2016-01-09 02:13:33 +0900

    Initial Commit

---
+++ MemePairingController/MemePairingController.h
@@ -0,0 +1,19 @@
+//
+//  MemePairingController.h
+//  MemePairingController
+//
+//  Created by MasashiShibata on 2016/01/09.
+//  Copyright © 2016年 c-bata. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MemePairingController.
+FOUNDATION_EXPORT double MemePairingControllerVersionNumber;
+
+//! Project version string for MemePairingController.
+FOUNDATION_EXPORT const unsigned char MemePairingControllerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MemePairingController/PublicHeader.h>
+
+
